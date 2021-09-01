194d1c73eb16f16cff4b9ec403b67bb6414455cb
Victor Lagoon <victorlagoon@victors-imac.lan>
2021-09-01 08:20:01 -0400

    API Start-up Issues

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -15,8 +15,8 @@ const routes: Routes = [
   {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server-Error'}},
   {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not-Found'}},
   {path: 'contact-us', component: ContactUsComponent},
-  {path: 'shop',  loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule),
-  data: {breadcrumb: 'Shop'}},
+  {path: 'basket',  loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule),
+  data: {breadcrumb: 'Basket'}},
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 
 ];  
