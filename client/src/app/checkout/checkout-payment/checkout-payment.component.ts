3ac6700eab17234a65af73a61ef507e83041c41d
Victor Lagoon <victorlagoon@victors-imac.lan>
2021-12-22 11:57:31 -0500

    End of section 8

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { NavigationExtras, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
@@ -7,13 +7,24 @@ import { IBasket } from 'src/app/shared/models/basket';
 import { IOrder } from 'src/app/shared/models/order';
 import { CheckoutService } from '../checkout.service';
 
+declare var Stripe;
+
 @Component({
   selector: 'app-checkout-payment',
   templateUrl: './checkout-payment.component.html',
   styleUrls: ['./checkout-payment.component.scss']
 })
-export class CheckoutPaymentComponent implements OnInit {
+export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
   @Input() checkoutForm: FormGroup;
+  @ViewChild('cardNumber', {static: true}) cardNumberElement: ElementRef;
+  @ViewChild('cardExpiry', {static: true}) cardExpiryElement: ElementRef;
+  @ViewChild('cardCvc', {static: true}) cardCvcElement: ElementRef;
+  stripe: any;
+  cardNumber: any;
+  cardExpiry: any;
+  cardCvc: any;
+  cardErrors: any;
+  cardHandler = this.onChange.bind(this);
 
   constructor(
     private basketService: BasketService,
@@ -21,8 +32,38 @@ export class CheckoutPaymentComponent implements OnInit {
     private toastr: ToastrService, 
     private router: Router) { }
 
-  ngOnInit(): void {
-  }      
+  ngAfterViewInit(): void {
+    this.stripe = Stripe('pk_test_51K0uBTK9EMg2SbXG0FsdK62f9K12D8BFjWwAY9w5fdWnqRd8EjgqWzUAGB12NjP898qH8r5rmArv3b8BsiGk0Aqy00eBZdi65R');
+    const elements = this.stripe.elements();
+
+    this.cardNumber = elements.create('cardNumber');
+    this.cardNumber.mount(this.cardNumberElement.nativeElement);
+    this.cardNumber.addEventListener('change', this.cardHandler);
+
+    this.cardExpiry = elements.create('cardExpiry');
+    this.cardExpiry.mount(this.cardExpiry.nativeElement);
+    this.cardExpiry.addEventListener('change', this.cardHandler);
+
+    this.cardCvc = elements.create('cardCvc');
+    this.cardCvc.mount(this.cardCvcElement.nativeElement);
+    this.cardCvc.addEventListener('change', this.cardHandler);
+    
+    
+  }   
+  
+  ngOnDestroy(): void {
+      this.cardNumber.destory();
+      this.cardExpiry.destory();
+      this.cardCvc.destory();
+  }
+
+  onChange({error}){
+    if (error){
+      this.cardErrors = error.message;
+    }else {
+      this.cardErrors = null;
+    }
+  }
 
   submitOrder() {
     const basket = this.basketService.getCurrentBasketValue();
